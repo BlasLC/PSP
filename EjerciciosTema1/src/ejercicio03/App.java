987aa2a0f3483bb68693be7b1f56647603c479a7
Blas López Calero <118302689+BlasLC@users.noreply.github.com>
2023-10-17 16:00:01 +0200

    Add files via upload

---
+++ EjerciciosTema1/src/ejercicio03/App.java
@@ -0,0 +1,10 @@
+package ejercicio03;
+
+import ejercicio02.EjecutarProceso;
+
+public class App {
+	public static void main(String[] args) {
+		EjecutarProceso proceso = new EjecutarProceso();
+		proceso.crearProceso();
+	}
+}
